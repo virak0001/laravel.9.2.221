f904644e8f012af7aa651677bb306e2fcf4f167e
virak0001 <virakcambodia44@gmail.com>
2021-09-03 20:11:19 +0700

    chore: config

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -131,6 +131,7 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="../resources/js/scripts.js"></script>
 </body>
 
 </html>
